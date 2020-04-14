1b1065fdbe78d9273abba25b5a01c4a83a2001e3
BauTancredi <bautistatancredi@hotmail.com>
2020-04-14 13:26:59 -0300

    fetchAPI useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Form from "./components/Form";
 
 function App() {
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      if (search === "") return;
+
+      const imagesPerPage = 30;
+      const url = `https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&q=${search}&per_page=${imagesPerPage}`;
+      const response = await fetch(url);
+      const result = await response.json();
+      console.log(result);
+    };
+    fetchAPI();
+  }, [search]);
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Image search engine</p>
-        <Form />
+        <Form setSearch={setSearch} />
       </div>
     </div>
   );
