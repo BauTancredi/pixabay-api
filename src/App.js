25b4e9f25eb293fae217eecd342f088ce9398774
BauTancredi <bautistatancredi@hotmail.com>
2020-04-14 13:11:17 -0300

    Form component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Form from "./components/Form";
 
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Image search engine</p>
+        <Form />
+      </div>
+    </div>
+  );
 }
 
 export default App;
